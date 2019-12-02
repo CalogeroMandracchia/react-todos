64a3dd10ae3c90ba112d003cfdcd095e6da00276
Calogero Mandracchia <calogero@efebia.com>
2019-12-02 01:17:32 +0100

    docs and register

---
+++ docs/precache-manifest.f8d4aab63259e58079fe8d8f16bcd8ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fe1b409e738971e8606cee2e84973c",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "4bc3d24940ea4ae47974",
+    "url": "/react-todos/static/css/main.1147353e.chunk.css"
+  },
+  {
+    "revision": "15386ee08ee03c7dfc2e",
+    "url": "/react-todos/static/js/2.62e1d1e9.chunk.js"
+  },
+  {
+    "revision": "4bc3d24940ea4ae47974",
+    "url": "/react-todos/static/js/main.7df538ec.chunk.js"
+  },
+  {
+    "revision": "d36f088eb41156318e6d",
+    "url": "/react-todos/static/js/runtime-main.f67cb468.js"
+  }
+]);
